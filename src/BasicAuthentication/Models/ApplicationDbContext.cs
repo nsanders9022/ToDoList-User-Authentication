a026babf8ec13cd7677cb343b6432ac049084d39
Alexandra Holcombe and Nicole Sanders <alexandra+nicole@>
2017-04-25 10:11:15 -0700

    to do list with user authentication

---
+++ src/BasicAuthentication/Models/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasicAuthentication.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+        public DbSet<Item> Items { get; set; }
+    }
+}
