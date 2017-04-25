8c4d1e477498ee7945784dae1dfedd9dc46b6ad1
Alexandra Holcombe and Nicole Sanders <alexandra+nicole@>
2017-04-25 16:25:12 -0700

    Add create roles form

--- src/BasicAuthentication/Controllers/AccountController.cs
+++ src/BasicAuthentication/Controllers/AccountController.cs
@@ -5,7 +5,6 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Identity;
 using BasicAuthentication.Models;
-using System.Threading.Tasks;
 using BasicAuthentication.ViewModels;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
---
+++ src/BasicAuthentication/Controllers/RolesController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Http;
+using BasicAuthentication.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BasicAuthentication.Controllers
+{
+    public class RolesController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public RolesController (ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(IdentityRole identityRole)
+        {
+            try
+            {
+                _db.Roles.Add(identityRole);
+                _db.SaveChanges();
+                ViewBag.ResultMessage = "Role created successfully!";
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
